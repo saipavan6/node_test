229935b65fa2998b5f56dacc9fb6de1e2ccc9b74
admin <pavan@perennialcode.in>
2024-11-24 20:32:43 +0530

    d

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/api/auth', emailRoutes);
 
 
 app.listen(PORT, () => {
-    console.log(Server is running on http://localhost:${PORT});
-    console.log(Swagger docs available at http://localhost:${PORT}/api-docs);
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
